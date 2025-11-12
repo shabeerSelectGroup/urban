9251620f107f5d1ca2fa3f020bdc07523259b44e
shabeerSelectGroup <shabeer@selectgroup.ae>
2025-11-12 10:25:10 +0400

    up2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/urban-navigator/',
+  base: '/Urban-Navigator/',
   plugins: [
     vue(),
     vueDevTools(),
